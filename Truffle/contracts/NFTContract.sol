eba64e3ee3a58e31e64d723ab51eba4d0f20da5f
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-12-26 23:54:27 +0000

    solidity code updates

--- Truffle/contracts/NFTContract.sol
+++ Truffle/contracts/NFTContract.sol
@@ -1,13 +1,10 @@
-pragma solidity ^0.8.0;
-
-//import 1155 token contract from openzeppelin
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-
 contract NFTContract is ERC1155, Ownable {
-
     uint256 public constant ARTWORK = 0;
     uint256 public constant PHOTO = 1;
 
@@ -32,5 +29,4 @@ contract NFTContract is ERC1155, Ownable {
         require(msg.sender == account);
         _burn(account, id, amount);
     }
-
 }
