1c36ce780f873be9d7b5f4d476441cfbc18a3467
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-12-26 02:19:39 +0000

    simplifying code

--- src/hooks/useNFTTokenIds.js
+++ src/hooks/useNFTTokenIds.js
@@ -1,7 +1,55 @@
 import { useMoralisDapp } from "providers/MoralisDappProvider/MoralisDappProvider";
 import { useEffect, useState } from "react";
+import { useMemo } from "react";
+import { useMoralis } from "react-moralis";
 import { useMoralisWeb3Api, useMoralisWeb3ApiCall } from "react-moralis";
 import { useIPFS } from "./useIPFS";
+
+export const useNFTTokenIds2 = (addr) => {
+    const { token } = useMoralisWeb3Api();
+    const { chainId, account } = useMoralis();
+    const { resolveLink } = useIPFS();
+    const getAllTokenIdsOpts = {
+        chain: chainId,
+        address: addr,
+        limit: 10,
+    };
+
+    const [totalNFTs, setTotalNFTs] = useState();
+    const [fetchSuccess, setFetchSuccess] = useState(true);
+
+    const {
+        fetch: getNFTTokenIds,
+        data,
+        error,
+        isLoading,
+        isFetching,
+      } = useMoralisWeb3ApiCall(token.getAllTokenIds, getAllTokenIdsOpts);
+
+      const NFTTokenIds = useMemo(() => {
+        if (!data?.result || !data?.result.length) {
+          return data;
+        }
+        setTotalNFTs(data.total);
+        const formattedResult = data.result.map((nft) => {
+          try {
+            if (nft.metadata) {
+              const metadata = JSON.parse(nft.metadata);
+              const image = resolveLink(metadata?.image);
+              return { ...nft, image, metadata };
+            }
+          } catch (error) {
+            setFetchSuccess(false);
+            return nft;
+          }
+          return nft;
+        });
+    
+        return { ...data, result: formattedResult };
+      }, [data]);
+    
+      return { getNFTTokenIds, data: NFTTokenIds, totalNFTs, fetchSuccess };
+}
 
 export const useNFTTokenIds = (addr) => {
     const { token } = useMoralisWeb3Api();
@@ -16,7 +64,8 @@ export const useNFTTokenIds = (addr) => {
         chain: chainId,
         address: addr,
         limit: 10,
-    }
+    };
+    console.log('getAllTokenIdsOpts', getAllTokenIdsOpts);
     const {
         fetch: getNFTTokenIds,
         data,
@@ -32,28 +81,46 @@ export const useNFTTokenIds = (addr) => {
     }, [addr]);
 
     useEffect(() => {
-        async function fetchData() {
-            await getNFTTokenIds();
-            if (data?.result) {
-                setDataFetched(true);
-                const NFTs = data.result;
-                setTotalNFTs(data.total);
-                setFetchSuccess(true);
-                for (const NFT of NFTs) {
-                    if (NFT?.metadata) {
-                        NFT.metadata = JSON.parse(NFT.metadata);
-                        NFT.image = resolveLink(NFT.metadata?.image);
-                    }
+        if (data?.result) {
+            setDataFetched(true);
+            const NFTs = data.result;
+            setTotalNFTs(data.total);
+            setFetchSuccess(true);
+            for (const NFT of NFTs) {
+                if (NFT?.metadata) {
+                    NFT.metadata = JSON.parse(NFT.metadata);
+                    NFT.image = resolveLink(NFT.metadata?.image);
                 }
-                setNFTTokenIds(NFTs);
             }
+            setNFTTokenIds(NFTs);
+        } else {
+            getNFTTokenIds(getAllTokenIdsOpts);
         }
-        if (addr !== "explore") {
-            console.log('dataFetched', dataFetched)
-            console.log('isLoading, isFetching, error', isLoading, isFetching, error);
-            if (!dataFetched) fetchData();
-        }
-    }, [data, addr]);
+    }, [data]);
+
+    // useEffect(() => {
+    //     async function fetchData() {
+    //         await getNFTTokenIds();
+    //         if (data?.result) {
+    //             setDataFetched(true);
+    //             const NFTs = data.result;
+    //             setTotalNFTs(data.total);
+    //             setFetchSuccess(true);
+    //             for (const NFT of NFTs) {
+    //                 if (NFT?.metadata) {
+    //                     NFT.metadata = JSON.parse(NFT.metadata);
+    //                     NFT.image = resolveLink(NFT.metadata?.image);
+    //                 }
+    //             }
+    //             setNFTTokenIds(NFTs);
+    //         }
+    //     }
+    //     if (addr !== "explore") {
+    //         console.log('dataFetched', dataFetched)
+    //         console.log('isLoading, isFetching, error', isLoading, isFetching, error);
+    //         if (!dataFetched) fetchData();
+    //     }
+    // }, [data, addr]);
 
     return {
         NFTTokenIds,
