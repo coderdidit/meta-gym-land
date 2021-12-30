46679666d89b45409a5791c1fd1e52833865fd69
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-12-30 20:02:47 +0000

    using ion-phaser/react

--- src/components/Play/games/GymRoom.jsx
+++ src/components/Play/games/GymRoom.jsx
@@ -1,12 +1,14 @@
+import React, { useState, useEffect } from "react";
 import Phaser from "phaser";
-// import GymRoomScene from "./GymRoomScene";
+import GymRoomScene from "./GymRoomScene";
+import { IonPhaser } from "@ion-phaser/react";
 
-const GymRoomID = "gym-room-canvas";
+// const GymRoomID = "gym-room-canvas";
 
 const getConfig = (scene) => {
     return {
         type: Phaser.AUTO,
-        parent: GymRoomID,
+        // parent: GymRoomID,
         width: 800,
         height: 600,
         mode: Phaser.Scale.FIT,
@@ -31,22 +33,44 @@ const getConfig = (scene) => {
 
 const GymRoom = () => {
     // run game
-    class GymRoomScene extends Phaser.Scene {
-        constructor() {
-            super({ key: 'gym-main-room' });
-        }
+    // class GymRoomScene extends Phaser.Scene {
+    //     constructor() {
+    //         super({ key: 'gym-main-room' });
+    //     }
 
-        preload() { }
+    //     preload() { }
 
-        create() { }
+    //     create() { }
 
-        update(time, delta) { }
-    }
-    const config = getConfig(GymRoomScene);
-    new Phaser.Game(config);
-    return (
-        <div id={GymRoomID} />
-    )
+    //     update(time, delta) { }
+    // }
+
+    const [initialised, setInitialised] = useState(true);
+    const [config, setConfig] = useState();
+
+    const startGame = () => {
+        setConfig({
+            ...getConfig(GymRoomScene),
+            callbacks: {
+                preBoot: (game) => {
+                    // Makes sure the game doesnt create another game on rerender
+                    setInitialised(false);
+                    // game.registry.merge({
+                    //   selectedGotchi,
+                    //   socket
+                    // });
+                },
+            },
+        });
+    };
+
+    useEffect(() => {
+        startGame();
+    }, []);
+
+    // const config = getConfig(GymRoomScene);
+    // new Phaser.Game(config);
+    return <IonPhaser initialize={initialised} game={config} id="phaser-app" />;
 };
 
 export default GymRoom;
