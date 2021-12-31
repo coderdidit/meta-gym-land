9e031990c402ff2f191a8219e321c60a05a3105c
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-12-31 00:55:55 +0000

    cleanup

--- src/components/Play/games/shared.js
+++ src/components/Play/games/shared.js
@@ -1,5 +1,3 @@
-// export const BG_KEY = "gym_floor";
-// export const LEFT_CHEVRON = 'left_chevron';
 export const PLAYER_KEY = "PLAYER";
 export const PLAYER_SCALE = 0.15;
 export const GYM_ROOM_SCENE = "gym-main-room";
