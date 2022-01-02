5d38bd22ad98e2aa71b0c705dac31e733089de53
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-01-02 01:06:28 +0000

    fixing bugs

--- src/components/Webcam/PoseDetWebcam.jsx
+++ src/components/Webcam/PoseDetWebcam.jsx
@@ -6,65 +6,70 @@ const PoseDetWebcam = ({ camTop, camLeft, camWidth }) => {
     const { webcamId, setWebcamId, webcamRef } = useContext(WebcamCtx);
     const canvasRef = useRef(null);
 
-    const reunPredictions = async () => {
+    const startPredictions = async () => {
         requestAnimationFrame(() => {
-            reunPredictions()
+            startPredictions();
         })
-        detect()
+        detectPose();
     };
 
     console.log('PoseDetWebcam webcamRef', webcamRef);
     console.log('PoseDetWebcam webcamId', webcamId);
 
-    const detect = async () => {
-        // Check data is available
-        if (
-            typeof webcamRef.current !== "undefined" &&
-            webcamRef.current !== null &&
-            webcamRef.current.video.readyState === 4 &&
-            canvasRef &&
-            canvasRef.current
-        ) {
+    const detectPose = async () => {
+        if (webCamAndCanvasAreInit()) {
+            doPredictionsCanvasSetup();
             // Get Video Properties
             const video = webcamRef.current.video;
-            const videoWidth = webcamRef.current.video.videoWidth;
-            const videoHeight = webcamRef.current.video.videoHeight;
-
-            // Set video width
-            // webcamRef.current.video.width = videoWidth;
-            // webcamRef.current.video.height = videoHeight;
-
-            // Set canvas height and width
-            canvasRef.current.width = videoWidth;
-            canvasRef.current.height = videoHeight;
-
-            // 4. TODO - Make Detections
-            // e.g. const obj = await net.detect(video);
-
             // Draw mesh
             const ctx = canvasRef.current.getContext("2d");
-            // ctx.drawImage(
-            //     video, 0, 0, videoWidth, videoHeight
-            // );
-
+            // testing
             ctx.fillStyle = '#FF0300';
             ctx.strokeStyle = '#FF0300';
             ctx.beginPath();
             ctx.arc(95, 50, 40, 0, 2 * Math.PI);
             ctx.stroke();
             ctx.fill();
-            // 5. TODO - Update drawing utility
-            // drawSomething(obj, ctx)  
         }
     };
 
-    useEffect(() => { reunPredictions() }, []);
+    const webCamAndCanvasAreInit = () => {
+        return typeof webcamRef.current !== "undefined" &&
+            webcamRef.current !== null &&
+            webcamRef.current.video.readyState === 4 &&
+            canvasRef &&
+            canvasRef.current
+    };
+
+    const doPredictionsCanvasSetup = () => {
+        // Get Video Properties
+        const videoWidth = webcamRef.current.video.videoWidth;
+        const videoHeight = webcamRef.current.video.videoHeight;
+
+        // Set video width
+        webcamRef.current.video.width = videoWidth;
+        webcamRef.current.video.height = videoHeight;
+        // Set canvas height and width
+        canvasRef.current.width = videoWidth;
+        canvasRef.current.height = videoHeight;
+    };
+
+    useEffect(() => {
+        startPredictions();
+    }, []);
+
+    const getVideoConstraints = () => {
+        if (webcamId) {
+            return { deviceId: webcamId }
+        }
+        return {}
+    }
 
     return (
         <>
             <Webcam
                 audio={false}
-                videoConstraints={{ deviceId: webcamId }}
+                videoConstraints={getVideoConstraints()}
                 mirrored={true}
                 className={"webcam"}
                 ref={webcamRef}
