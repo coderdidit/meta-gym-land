3bde69b72f52a67ec7cb205f6859bbc980067bc4
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-01-02 16:59:43 +0000

    something working with mediapipe

--- src/components/Webcam/PoseDetWebcam.jsx
+++ src/components/Webcam/PoseDetWebcam.jsx
@@ -1,35 +1,83 @@
 import React, { useContext, useRef, useEffect } from "react";
-import { WebcamCtx } from "index";
+import { WebcamCtx, PoseDetectorCtx } from "index";
 import Webcam from "react-webcam";
+import { Camera } from "@mediapipe/camera_utils";
+import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
+import { POSE_CONNECTIONS } from '@mediapipe/pose';
+
 
 const PoseDetWebcam = ({ styleProps }) => {
     const { webcamId, setWebcamId, webcamRef } = useContext(WebcamCtx);
+    const { poseDetector } = useContext(PoseDetectorCtx);
     const canvasRef = useRef(null);
 
-    const startPredictions = async () => {
-        requestAnimationFrame(() => {
-            startPredictions();
-        })
-        detectPose();
-    };
-
+    console.log('poseDetector PoseDetWebcam', poseDetector);
     console.log('PoseDetWebcam webcamRef', webcamRef);
     console.log('PoseDetWebcam webcamId', webcamId);
 
-    const detectPose = async () => {
+    const startPredictions = () => {
+        // requestAnimationFrame(() => {
+        //     startPredictions();
+        // })
+        // detectPose();
+        const videoElement = webcamRef.current.video;
+
+        poseDetector.onResults(onResults);
+        // limit fps for predictions
+        const fps = 15;
+        const interval = 1000 / fps;
+        let then = Date.now();
+        const camera = new Camera(videoElement, {
+            onFrame: async () => {
+                if (webCamAndCanvasAreInit()) {
+                    const now = Date.now();
+                    const delta = now - then;
+                    if (delta > interval) {
+                        then = now - (delta % interval);
+                        // run prediction
+                        await poseDetector.send({ image: videoElement });
+                    }
+                }
+            },
+            // width: 1280,
+            // height: 720
+        });
+        camera.start();
+    };
+
+    const onResults = (results) => {
         if (webCamAndCanvasAreInit()) {
             doPredictionsCanvasSetup();
+            console.log('onResults', results);
+
             // Get Video Properties
-            const video = webcamRef.current.video;
+            // const video = webcamRef.current.video;
+            const canvasCtx = canvasRef.current.getContext("2d");
+            canvasCtx.save();
+            canvasCtx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+
+            // Only overwrite existing pixels.
+            canvasCtx.globalCompositeOperation = 'source-in';
+            canvasCtx.fillStyle = '#00FF00';
+            canvasCtx.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+            // Only overwrite missing pixels.
+            canvasCtx.globalCompositeOperation = 'destination-atop';
+
+            // Draw testing circle
+            canvasCtx.fillStyle = '#04AA6D';
+            canvasCtx.strokeStyle = '#04AA6D';
+            canvasCtx.beginPath();
+            canvasCtx.arc(50, 50, 20, 0, 2 * Math.PI);
+            canvasCtx.stroke();
+            canvasCtx.fill();
+
             // Draw mesh
-            const ctx = canvasRef.current.getContext("2d");
-            // testing
-            ctx.fillStyle = '#04AA6D';
-            ctx.strokeStyle = '#04AA6D';
-            ctx.beginPath();
-            ctx.arc(50, 50, 20, 0, 2 * Math.PI);
-            ctx.stroke();
-            ctx.fill();
+            canvasCtx.globalCompositeOperation = 'source-over';
+            drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
+                { color: '#00FF00', lineWidth: 4 });
+            drawLandmarks(canvasCtx, results.poseLandmarks,
+                { color: '#FF0000', lineWidth: 4 });
+            canvasCtx.restore();
         }
     };
 
