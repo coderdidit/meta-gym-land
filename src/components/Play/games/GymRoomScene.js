84aa16c0dc6f2d5f82c2b070e6bfced5743d9371
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-01-04 23:25:29 +0000

    working

--- src/components/Play/games/GymRoomScene.js
+++ src/components/Play/games/GymRoomScene.js
@@ -2,13 +2,13 @@ import Phaser from "phaser";
 import { getGameWidth, getGameHeight, getRelative } from "./helpers";
 import { Player } from "./objects";
 import { PLAYER_KEY, PLAYER_SCALE, GYM_ROOM_SCENE } from "./shared";
-import { 
-    LEFT_CHEVRON, 
+import {
+    LEFT_CHEVRON,
     BG,
     GYM_ROOM_MAP,
     GYM_ROOM_TILES,
     GYM_ROOM_MAT_SKY,
-    GYM_ROOM_MAT_SPACE, 
+    GYM_ROOM_MAT_SPACE,
 
     GYM_ROOM_DANGEON_MAP,
     GYM_ROOM_DANGEON_TILES,
@@ -20,7 +20,7 @@ const SceneConfig = {
     key: GYM_ROOM_SCENE,
 };
 
-const mapScale = 1.1
+const mapScale = 1.2
 
 export class GymRoomScene extends Phaser.Scene {
     constructor() {
@@ -38,19 +38,20 @@ export class GymRoomScene extends Phaser.Scene {
         const height = getGameHeight(this);
 
         // bg image
-        // this.add.image(width / 2, height / 2, BG)
-        //     .setDisplaySize(width, height);
+        this.add.image(width / 2, height / 2, BG)
+            .setDisplaySize(width, height);
 
         // map
         const map = this.make.tilemap({ key: GYM_ROOM_DANGEON_MAP })
 
         const tileset_main = map.addTilesetImage('dangeon', GYM_ROOM_DANGEON_TILES, 16, 16)
-        const groundLayer = map.createLayer('floor', tileset_main)
-        const wallsLayer = map.createLayer('walls', tileset_main)
-        groundLayer.setScale(mapScale).setOrigin({x: width / 2,
-            y: height / 2})
-        wallsLayer.setScale(mapScale).setOrigin({x: width / 2,
-            y: height / 2})
+        const groundLayer = map
+            .createLayer('floor', tileset_main, (width / 4), 0)
+
+        const wallsLayer = map
+            .createLayer('walls', tileset_main, (width / 4), 0)
+        groundLayer.setScale(mapScale)
+        wallsLayer.setScale(mapScale)
 
         // back btn   
         // uncomment if you want to have sound on exit
