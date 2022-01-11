5625a6e5b153af74f3c80e16eb746758c7257e32
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-01-11 23:30:28 +0000

    simplify

--- src/components/NFTBalance.jsx
+++ src/components/NFTBalance.jsx
@@ -109,8 +109,7 @@ function NFTBalance() {
           <b><u>Marketplace</u></b>
         </Link>{" "}🚀
       </h3>
-      {NFTBalances?.result && NFTBalances.result.length > 0 &&
-        <Divider style={{ backgroundColor: brightFontCol }} />}
+      <Divider style={{ backgroundColor: brightFontCol }} />
       <div style={NFTsDiv}>
         <Skeleton loading={!NFTBalances?.result}>
           {NFTBalances?.result &&
