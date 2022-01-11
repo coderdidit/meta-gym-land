2793c8c9c7a96b273ca6e20ac12127cee54a3e10
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-01-11 02:02:29 +0000

    parsing results logic wip

--- src/components/gpose/functions.js
+++ src/components/gpose/functions.js
@@ -1,3 +1,6 @@
+import { ConfidenceScore } from "../../AIConfig";
+import { getAngleBetween } from './angles';
+import * as pose from "./pose";
 
 // for media pipe
 const mapResults = (results) => {
@@ -17,6 +20,8 @@ const mapMediaPipeResults = (results) => {
     // remeber this is in selfie mode
     return {
         nose: posLandXY[0],
+        leftShoulder: posLandXY[11],
+        rightShoulder: posLandXY[12],
         leftElbow: posLandXY[13],
         rightElbow: posLandXY[14],
         leftEye: posLandXY[3], // 1, 2, 3 (inner, eye, outer)
@@ -29,7 +34,50 @@ const mapMediaPipeResults = (results) => {
 
 const resToGPose = (results) => {
     const parsedRes = mapResults(results);
-    return '';
+
+    const { nose, leftEye, rightEye,
+        leftShoulder, rightShoulder,
+        leftElbow, rightElbow } = parsedRes;
+
+
+    // TODO investigate more how this logic works in terms of flipping
+    const leftElbowToSholder = getAngleBetween(leftElbow, leftShoulder) * -1
+    const rightElbowToSholder = getAngleBetween(rightShoulder, rightElbow)
+
+    const angle = 52
+
+    const bothArmsUp = (leftElbowToSholder > angle)
+        && (rightElbowToSholder > angle)
+    const moveDown = (leftElbowToSholder > angle
+        && rightElbowToSholder < angle) && !bothArmsUp
+    const moveUp = (rightElbowToSholder > angle
+        && leftElbowToSholder < angle) && !bothArmsUp
+
+    const noseToLeftEyeYdistance = nose.y - leftEye.y
+    const noseToRightEyeYdistance = nose.y - rightEye.y
+
+    // vissibility TODO treshold settings here may be unecessary
+    const scoreThreshold = ConfidenceScore;
+    const noseVissible = nose.score > scoreThreshold
+    const lEVissible = leftEye.score > scoreThreshold
+    const REVissible = rightEye.score > scoreThreshold
+    const moveSideActivationDist = 8;
+
+    if (noseVissible && lEVissible
+        && noseToLeftEyeYdistance < moveSideActivationDist) {
+        return pose.HTL;
+    } else if (noseVissible && REVissible
+        && noseToRightEyeYdistance < moveSideActivationDist) {
+        return pose.HTR;
+    } else if (bothArmsUp) {
+        return pose.BA_UP;
+    } else if (moveUp) {
+        return pose.LA_UP;
+    } else if (moveDown) {
+        return pose.RA_UP;
+    } else {
+        return idle;
+    }
 };
 
 export { resToGPose };
