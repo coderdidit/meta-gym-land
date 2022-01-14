ed6d3910b52e32531041397fd67f5487d87e97fb
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-01-14 20:15:21 +0000

    cleanup

--- src/components/Marketplace/collections.js
+++ src/components/Marketplace/collections.js
@@ -1,23 +1,17 @@
 import { AllowedNftContracts } from "../../MglNftMetadata";
 
 const AvaxTestnetChainID = "0xa869";
-// const RinkebyTestnet = "0x4";
 
 export const networkCollections = new Map([
-  [AvaxTestnetChainID, [ // Avalanche Testnet
-    {
-      image: "https://ipfs.moralis.io:2053/ipfs/QmVF53rCjFiFSXyJd64NgeGioQG93gegdsymyMWtJLG9Ev/cover.gif",
-      name: "Moralis Avalanche Hackaton 2021 Test Drop",
-      addrs: AllowedNftContracts.get(AvaxTestnetChainID),
-    },
-  ]],
-  // [RinkebyTestnet, [ // rinkeby
-  //   {
-  //     image: "https://ipfs.moralis.io:2053/ipfs/QmVF53rCjFiFSXyJd64NgeGioQG93gegdsymyMWtJLG9Ev/cover.gif",
-  //     name: "Moralis Avalanche Hackaton 2021 Test Drop",
-  //     addrs: AllowedNftContracts.get(RinkebyTestnet),
-  //   },
-  // ]]
+  [AvaxTestnetChainID,
+    [
+      {
+        image: "https://ipfs.moralis.io:2053/ipfs/QmVF53rCjFiFSXyJd64NgeGioQG93gegdsymyMWtJLG9Ev/cover.gif",
+        name: "Moralis Avalanche Hackaton 2021 Test Drop",
+        addrs: AllowedNftContracts.get(AvaxTestnetChainID),
+      },
+    ]
+  ],
 ]);
 
 export const getCollectionsByChain = (chain) => networkCollections.get(chain);
