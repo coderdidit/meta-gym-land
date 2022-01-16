93879862c713fcb01aba96e9b24f7d3577d8f60c
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-01-16 02:30:46 +0000

    wip

--- src/components/LoaderTest.jsx
+++ src/components/LoaderTest.jsx
@@ -2,17 +2,7 @@ import Loader from "./Loader";
 
 const LoaderTest = () => {
     return (
-        <Loader
-            style={{
-                position: "absolute",
-                zIndex: "1",
-                paddingTop: "20%",
-                paddingLeft: "50%",
-                background: "rgba(0, 0, 0, 0.09)",
-                width: "100%",
-                height: "100%",
-            }}
-        />
+        <Loader />
     );
 }
 
