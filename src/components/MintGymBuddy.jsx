eb5630079e66b905b5a99adc6b3446d18a175491
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-05-23 22:16:06 +0200

    feat: add dummy button

--- src/components/MintGymBuddy.jsx
+++ src/components/MintGymBuddy.jsx
@@ -1,10 +1,29 @@
 import {
     pageTitleStyle,
-    descriptionStyle
+    descriptionStyle,
+    BtnPrimary,
 } from "GlobalStyles";
+import { getExplorer } from "helpers/networks";
 import { SmileFilled } from "@ant-design/icons";
+import { TestGymBuddiesContract, MainChainID } from "../MglNftMetadata";
+import { SelectOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+import {
+    // useMoralis,
+    // useMoralisQuery,
+    useWeb3ExecuteFunction
+} from "react-moralis";
 
 const MintGymBuddyPage = () => {
+
+    const contractProcessor = useWeb3ExecuteFunction();
+    const contractAddress = TestGymBuddiesContract;
+    const chainId = MainChainID;
+
+    const handleMintClick = () => {
+        alert('should mint');
+    }
+
     return (
         <div style={{
             textAlign: "center",
@@ -20,8 +39,24 @@ const MintGymBuddyPage = () => {
                     ...descriptionStyle,
                     padding: "1rem 0",
                 }}>
-                    ... test description
+                    <div style={{ marginTop: "10px", padding: "0 10px" }}>
+                        <a
+                            style={{ color: "ivory" }}
+                            href={`${getExplorer(chainId)}/address/${contractAddress}`}
+                            target="_blank" rel="noreferrer"
+                        >
+                            <SelectOutlined style={{ marginRight: "5px" }} />
+                            View GymBuddies NFT Contract on Explorer
+                        </a>
+                    </div>
                 </div>
+                <Button
+                    type="primary"
+                    style={BtnPrimary}
+                    onClick={handleMintClick}
+                >
+                    Mint
+                </Button>
             </div>
         </div>
     )
