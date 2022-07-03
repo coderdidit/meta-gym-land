61da7a3ed17221ba6bc104be7b0020f3a50cb4a8
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-07-03 22:04:01 +0200

    feat: add action for new snap btn

--- src/components/DemoAvatar.jsx
+++ src/components/DemoAvatar.jsx
@@ -175,7 +175,6 @@ function DemoAvatar() {
             return (
               <Card
                 key={index}
-                hoverable
                 actions={[
                   <Tooltip title="View On Blockexplorer">
                     <FileSearchOutlined
@@ -225,25 +224,59 @@ function DemoAvatar() {
                           }}
                         />
                       </div>
-                      <div
-                        style={{
-                          backgroundColor: "#F6F403",
-                          color: "black",
-                          zIndex: "2",
-                          margin: "1rem",
-                          padding: "0.5rem",
-                          borderRadius: "50%",
-                          border: "1px solid black",
-                          height: "42px",
-                          width: "42px",
-                          // grid props
-                          gridArea: "overlap",
-                          alignSelf: "start",
-                          justifySelf: "end",
-                        }}
-                      >
-                        <SnapChatLogo width={24} height={24} />
-                      </div>
+                      {queryParamFlag() && (
+                        <div
+                          className="snap-btn"
+                          style={{
+                            backgroundColor: "#F6F403",
+                            color: "black",
+                            zIndex: "2",
+                            margin: "1rem",
+                            padding: "0.5rem",
+                            borderRadius: "50%",
+                            border: "1px solid black",
+                            height: "42px",
+                            width: "42px",
+                            // grid props
+                            gridArea: "overlap",
+                            alignSelf: "start",
+                            justifySelf: "end",
+                          }}
+                          onClick={() => {
+                            Modal.info({
+                              title: "Try me in Snapchat",
+                              centered: true,
+                              bodyStyle: {
+                                textAlign: "center",
+                              },
+                              okText: "close",
+                              icon: <SnapChatLogo />,
+                              content: (
+                                <div
+                                  style={{
+                                    textAlign: "center",
+                                  }}
+                                >
+                                  <div>
+                                    <p>Grab your phone</p>
+                                    <p>and scan the QR code</p>
+                                  </div>
+                                  <div
+                                    style={{
+                                      display: "flex",
+                                      justifyContent: "center",
+                                    }}
+                                  >
+                                    <img src={snapQRCodeDataURI} />
+                                  </div>
+                                </div>
+                              ),
+                            });
+                          }}
+                        >
+                          <SnapChatLogo width={24} height={24} />
+                        </div>
+                      )}
                     </div>
                     <Badge.Ribbon
                       text="I will disappear soon"
@@ -285,58 +318,6 @@ function DemoAvatar() {
                   >
                     <Link to="/play-setup">Play with me</Link>
                   </Button>
-                </div>
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    marginTop: "1rem",
-                  }}
-                >
-                  {queryParamFlag() && (
-                    <Button
-                      style={{
-                        ...BtnPrimary,
-                        backgroundColor: "#F6F403",
-                        color: "black",
-                        border: "1px solid black",
-                      }}
-                      onClick={() => {
-                        Modal.info({
-                          title: "Try me in Snapchat",
-                          centered: true,
-                          bodyStyle: {
-                            textAlign: "center",
-                          },
-                          okText: "close",
-                          icon: <SnapChatLogo />,
-                          content: (
-                            <div
-                              style={{
-                                textAlign: "center",
-                              }}
-                            >
-                              <div>
-                                <p>Grab your phone</p>
-                                <p>and scan the QR code</p>
-                              </div>
-                              <div
-                                style={{
-                                  display: "flex",
-                                  justifyContent: "center",
-                                }}
-                              >
-                                <img src={snapQRCodeDataURI} />
-                              </div>
-                            </div>
-                          ),
-                        });
-                      }}
-                    >
-                      Try me in Snapchat
-                    </Button>
-                  )}
                 </div>
               </Card>
             );
