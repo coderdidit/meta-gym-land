e13f80d812d4012e936f631d1dc1948df731229f
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-07-11 00:31:06 +0200

    feat: update smartcontract addresses

--- src/MglNftMetadata.js
+++ src/MglNftMetadata.js
@@ -1,10 +1,10 @@
 export const TestGymBuddiesContract =
   "0xaf442b8249278126cc04038079a43f1721f48e1e";
 export const TestGymBuddiesWithSnapLensContract =
-  "0xaf442b8249278126cc04038079a43f1721f48e1e";
+  "0xA52237FdB244BE432A5faf411E10BEf96a14eD9e";
 export const AvaxMoralis2021HackatonNfts =
   "0xbba97ea3912c598a39ce0802d5cd67dd3d873457";
-export const DemoNFT = "0x55bccbaf3d0cfd33b634c9c0df09ba2c14cc7c2a";
+export const DemoNFT = "0xC2718Fbe99e408740eBad565334F6c8d8570B9AF";
 export const AllowedNftContracts = new Map([
   [
     "0xa869", // AVAX fuji testnet
