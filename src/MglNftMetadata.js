8d5661d17ff1afaf0573c60c406c6ebe186d64d3
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-07-10 23:05:44 +0200

    feat: support additional contract with snap lens links

--- src/MglNftMetadata.js
+++ src/MglNftMetadata.js
@@ -1,12 +1,14 @@
 export const TestGymBuddiesContract =
   "0xaf442b8249278126cc04038079a43f1721f48e1e";
+export const TestGymBuddiesWithSnapLensContract =
+  "0xaf442b8249278126cc04038079a43f1721f48e1e";
 export const AvaxMoralis2021HackatonNfts =
   "0xbba97ea3912c598a39ce0802d5cd67dd3d873457";
 export const DemoNFT = "0x55bccbaf3d0cfd33b634c9c0df09ba2c14cc7c2a";
 export const AllowedNftContracts = new Map([
   [
     "0xa869", // AVAX fuji testnet
-    [TestGymBuddiesContract],
+    [TestGymBuddiesContract, TestGymBuddiesWithSnapLensContract],
   ],
 ]);
 
