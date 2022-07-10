148ea3093bbc7b76d534d54718ec6b9606dc3d98
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-07-11 01:35:25 +0200

    feat: update contract

--- src/MglNftMetadata.js
+++ src/MglNftMetadata.js
@@ -1,7 +1,7 @@
 export const TestGymBuddiesContract =
   "0xaf442b8249278126cc04038079a43f1721f48e1e";
 export const TestGymBuddiesWithSnapLensContract =
-  "0xA52237FdB244BE432A5faf411E10BEf96a14eD9e";
+  "0x68CB9498fA3346F6fB186073FeF9c933FF5d2e35";
 export const AvaxMoralis2021HackatonNfts =
   "0xbba97ea3912c598a39ce0802d5cd67dd3d873457";
 export const DemoNFT = "0xC2718Fbe99e408740eBad565334F6c8d8570B9AF";
