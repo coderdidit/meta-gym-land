acf03141f2fd1393f4e2b0d51a751c8721e52190
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-07-29 18:57:35 +0200

    chore: move to typescript in progress

--- src/games/minigames/invaders/InvadersScene.ts
+++ src/games/minigames/invaders/InvadersScene.ts
@@ -4,7 +4,7 @@ import { PlayerWithName } from "../../objects";
 import { INVADERS } from "../../shared";
 import { createTextBox } from "../../utils/text";
 import { mainBgColorNum, highlightTextColorNum } from "../../../GlobalStyles";
-import { SceneInMetaGymRoom } from "../../base-scenes/scene-in-metagym-room";
+import { SceneInMetaGymRoom } from "../../base-scenes/ts/scene-in-metagym-room";
 
 const SceneConfig = {
   active: false,
@@ -13,6 +13,9 @@ const SceneConfig = {
 };
 
 export class InvadersScene extends SceneInMetaGymRoom {
+  // TODO define type
+  player: any;
+
   constructor() {
     super(SceneConfig);
   }
