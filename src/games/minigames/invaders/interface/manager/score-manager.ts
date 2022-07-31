dd774499fa579a4c4ff01df595b027044fdffe84
Martin Kuzdowicz (CoderDidit) <86926500+coderdidit@users.noreply.github.com>
2022-07-31 21:43:27 +0100

    feat: basic invaders game (#139)
    * feat: working basic invaders game
    
    * feat: initial game scaling
    
    * chore: remove unused var
    
    * chore: scaling game
    
    * fix: fix animation
    
    * feat: manage font for space invader
    
    * feat: add gym buddy to invaders game
    
    * feat: decrease score values
    
    * feat: game over text
    
    * feat: gym buddy shoots
    
    * feat: implement flip flop in invaders
    
    * feat: adjusint flip flop on rush scene
    
    * feat: adjust scaling in rush minigame
    
    * chore: version top-up
    
    * chore: remove unused asset
    
    * chore: remove console log
    
    Co-authored-by: mkuzdowicz <martin.kuzdowicz@gmail.com>

---
+++ src/games/minigames/invaders/interface/manager/score-manager.ts
@@ -0,0 +1,138 @@
+import { getGameWidth } from "games/helpers";
+import { InGameFont } from "GlobalStyles";
+import { PLAYER_KEY } from "games/shared";
+
+export class ScoreManager {
+  scoreText!: Phaser.GameObjects.Text;
+  line1Text!: Phaser.GameObjects.Text;
+  line2Text!: Phaser.GameObjects.Text;
+  lives!: Phaser.Physics.Arcade.Group;
+
+  get noMoreLives() {
+    return this.lives.countActive(true) === 0;
+  }
+
+  highScore = 0;
+  score = 0;
+
+  constructor(private _scene: Phaser.Scene) {
+    this._init();
+    this.print();
+  }
+
+  private _init() {
+    const textConfig = {
+      fontFamily: InGameFont,
+      fill: "#ffffff",
+    };
+    const normalTextConfig = {
+      ...textConfig,
+      fontSize: "16px",
+    };
+
+    const bigTextConfig = {
+      ...textConfig,
+      fontSize: "36px",
+    };
+
+    // basic props
+    const width = getGameWidth(this._scene);
+
+    const scoreX = width * 0.06;
+    this._scene.add.text(scoreX, 16, `SCORE`, normalTextConfig);
+    this.scoreText = this._scene.add.text(scoreX, 32, "", normalTextConfig);
+
+    // ending text
+    this.line1Text = this._scene.add
+      .text(width / 2, 320, "", bigTextConfig)
+      .setOrigin(0.5);
+
+    this.line2Text = this._scene.add
+      .text(width / 2, 400, "", bigTextConfig)
+      .setOrigin(0.5);
+
+    // lives text
+    this._setLivesText(width, normalTextConfig);
+  }
+
+  private _setLivesText(
+    SIZE_X: number,
+    textConfig: { fontSize: string; fontFamily: string; fill: string },
+  ) {
+    this._scene.add.text(SIZE_X - 100, 16, `LIVES`, textConfig);
+    this.lives = this._scene.physics.add.group({
+      maxSize: 3,
+      runChildUpdate: true,
+    });
+    this.resetLives();
+  }
+
+  resetLives() {
+    const SIZE_X = getGameWidth(this._scene);
+    this.lives.clear(true, true);
+    for (let i = 0; i < 3; i++) {
+      const ship: Phaser.GameObjects.Sprite = this.lives.create(
+        SIZE_X - 125 + 45 * i,
+        60,
+        PLAYER_KEY,
+      );
+      ship.setScale(0.5);
+      ship.setOrigin(0.5, 0.5);
+      // ship.setAngle(90);
+      ship.setAlpha(0.6);
+    }
+  }
+
+  setWinText() {
+    this._setBigText("YOU WON!", "PRESS SPACE FOR NEW GAME");
+  }
+
+  setGameOverText() {
+    this._setBigText("GAME OVER", "PRESS SPACE FOR NEW GAME");
+  }
+
+  hideText() {
+    this.line1Text.setText("").setPadding(0).setDepth(0);
+    this.line2Text.setText("").setPadding(0).setDepth(0);
+  }
+
+  private _setBigText(line1: string, line2: string) {
+    const depth = 2;
+    const bgColor = "#FFFFFF";
+    const textColor = "#030404";
+    const padding = 8;
+    this.line1Text
+      .setText(line1)
+      .setBackgroundColor(bgColor)
+      .setColor(textColor)
+      .setPadding(padding)
+      .setDepth(depth);
+    this.line2Text
+      .setText(line2)
+      .setBackgroundColor(bgColor)
+      .setColor(textColor)
+      .setPadding(padding)
+      .setDepth(depth);
+  }
+
+  setHighScore() {
+    if (this.score > this.highScore) {
+      this.highScore = this.score;
+    }
+    this.score = 0;
+    this.print();
+  }
+
+  print() {
+    this.scoreText.setText(`${this.padding(this.score)}`);
+  }
+
+  increaseScore(step = 1) {
+    this.score += step;
+    this.print();
+  }
+
+  padding(num: number) {
+    return `${num}`.padStart(4, "0");
+  }
+}
