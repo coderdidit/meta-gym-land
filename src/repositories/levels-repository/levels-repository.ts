bf35c419641a7d7132985828539b76bd930582a5
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-08-16 23:32:05 +0200

    feat: levels repo

---
+++ src/repositories/levels-repository/levels-repository.ts
@@ -0,0 +1,26 @@
+/**
+ * TODO: put to Supabase or Firebase
+ */
+export { nameToId, IdToName };
+
+const trial = "Trial";
+const beginner = "Beginner";
+const athlete = "Athlete";
+const seniorAthlete = "Senior Athlete";
+const mysterySolver = "Mystery Solver";
+
+const nameToId = new Map<string, number>([
+  [trial, 0],
+  [beginner, 1],
+  [athlete, 2],
+  [seniorAthlete, 3],
+  [mysterySolver, 4],
+]);
+
+const IdToName = new Map<number, string>([
+  [0, trial],
+  [1, beginner],
+  [2, athlete],
+  [3, seniorAthlete],
+  [4, mysterySolver],
+]);
