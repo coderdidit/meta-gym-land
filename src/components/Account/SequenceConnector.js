afe04011997f98acd8320827406fb0bca730c57f
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-08-21 12:36:30 +0200

    feat: integrate sequence wallet

---
+++ src/components/Account/SequenceConnector.js
@@ -0,0 +1,123 @@
+import { AbstractWeb3Connector } from "moralis";
+import { sequence } from "0xsequence";
+import { debugLog } from "dev-utils/debug";
+
+class NoEthereumProviderError extends Error {
+  constructor() {
+    super();
+    this.message = "Non ethereum enabled browser";
+  }
+}
+
+// async function getProvider() {
+//     // 0x13881 // Polygon Mumbai testnet
+//     const wallet = await sequence.initWallet("0x13881");
+//     debugLog('[SequenceConnector] wallet init', wallet);
+//     const provider = wallet.getProvider();
+//     if (provider === undefined) {
+//         throw Error("failed to get sequence wallet provider");
+//     }
+//     return provider;
+// }
+
+function getProvider() {
+  if (!window?.sequence.isSequence) {
+    window.open("https://sequence.xyz");
+    return null;
+  }
+  return window.sequence;
+}
+
+function fromDecimalToHex(number) {
+  if (typeof number !== "number") throw "The input provided should be a number";
+  return `0x${number.toString(16)}`;
+}
+
+/**
+ * Converts chainId to a hex if it is a number
+ */
+function verifyChainId(chainId) {
+  if (typeof chainId === "number") chainId = fromDecimalToHex(chainId);
+  return chainId;
+}
+
+class SequenceConnector extends AbstractWeb3Connector {
+  type = "sequence";
+
+  verifyEthereumBrowser() {
+    if (!window?.ethereum) {
+      throw new NoEthereumProviderError();
+    }
+  }
+
+  async activate() {
+    this.verifyEthereumBrowser();
+
+    const provider = getProvider();
+
+    debugLog("[SequenceConnector]", provider);
+
+    const [accounts, chainId] = await Promise.all([
+      provider.request({ method: "eth_requestAccounts" }),
+      provider.request({ method: "eth_chainId" }),
+    ]);
+
+    const account = accounts[0] ? accounts[0].toLowerCase() : null;
+
+    this.chainId = verifyChainId(chainId);
+    this.account = account;
+    this.provider = provider;
+
+    this.subscribeToEvents(provider);
+
+    return { provider, chainId, account };
+  }
+
+  async switchNetwork(chainId) {
+    this.verifyEthereumBrowser();
+    chainId = verifyChainId(chainId);
+
+    const currentNetwork = this.chainId;
+    if (currentNetwork === chainId) return;
+
+    const provider = getProvider();
+
+    await provider.request({
+      method: "wallet_switchEthereumChain",
+      params: [{ chainId }],
+    });
+  }
+
+  async addNetwork(
+    chainId,
+    chainName,
+    currencyName,
+    currencySymbol,
+    rpcUrl,
+    blockExplorerUrl,
+  ) {
+    this.verifyEthereumBrowser();
+
+    const newchainId = verifyChainId(chainId);
+    const provider = getProvider();
+
+    await provider.request({
+      method: "wallet_addEthereumChain",
+      params: [
+        {
+          chainId: newchainId,
+          chainName: chainName,
+          nativeCurrency: {
+            name: currencyName,
+            symbol: currencySymbol,
+            decimals: 18,
+          },
+          rpcUrls: [rpcUrl],
+          blockExplorerUrls: blockExplorerUrl ? [blockExplorerUrl] : null,
+        },
+      ],
+    });
+  }
+}
+
+export default SequenceConnector;
--- src/components/Account/config.js
+++ src/components/Account/config.js
@@ -1,12 +1,14 @@
 import Metamask from "./WalletIcons/metamaskWallet.png";
-import Coin98 from "./WalletIcons/Coin98.png";
 import WalletConnect from "./WalletIcons/wallet-connect.svg";
-import MathWallet from "./WalletIcons/MathWallet.svg";
-import TokenPocket from "!file-loader!./WalletIcons/TokenPocket.svg";
-import SafePal from "!file-loader!./WalletIcons/SafePal.svg";
+// import MathWallet from "./WalletIcons/MathWallet.svg";
+// import TokenPocket from "!file-loader!./WalletIcons/TokenPocket.svg";
+// import SafePal from "!file-loader!./WalletIcons/SafePal.svg";
+// import Coin98 from "./WalletIcons/Coin98.png";
 import TrustWallet from "./WalletIcons/TrustWallet.png";
+import SequenceWallet from "./WalletIcons/sequence.svg";
 import BitKeepWallet from "./WalletIcons/bkWallet.png";
 import BitKeepConnector from "./BitKeepConnector";
+import SequenceConnector from "./SequenceConnector";
 
 export const connectors = [
   {
@@ -22,10 +24,10 @@ export const connectors = [
     priority: 2,
   },
   {
-    title: "BitKeep Wallet",
-    icon: BitKeepWallet,
-    connectorId: "bitkeep",
-    customConnector: BitKeepConnector,
+    title: "Sequence Wallet",
+    icon: SequenceWallet,
+    connectorId: "sequence",
+    customConnector: SequenceConnector,
     priority: 3,
   },
   {
@@ -35,27 +37,34 @@ export const connectors = [
     priority: 4,
   },
   {
-    title: "MathWallet",
-    icon: MathWallet,
-    connectorId: "injected",
-    priority: 999,
+    title: "BitKeep Wallet",
+    icon: BitKeepWallet,
+    connectorId: "bitkeep",
+    customConnector: BitKeepConnector,
+    priority: 5,
   },
-  {
-    title: "TokenPocket",
-    icon: TokenPocket,
-    connectorId: "injected",
-    priority: 999,
-  },
-  {
-    title: "SafePal",
-    icon: SafePal,
-    connectorId: "injected",
-    priority: 999,
-  },
-  {
-    title: "Coin98",
-    icon: Coin98,
-    connectorId: "injected",
-    priority: 999,
-  },
+  // {
+  //   title: "MathWallet",
+  //   icon: MathWallet,
+  //   connectorId: "injected",
+  //   priority: 999,
+  // },
+  // {
+  //   title: "TokenPocket",
+  //   icon: TokenPocket,
+  //   connectorId: "injected",
+  //   priority: 999,
+  // },
+  // {
+  //   title: "SafePal",
+  //   icon: SafePal,
+  //   connectorId: "injected",
+  //   priority: 999,
+  // },
+  // {
+  //   title: "Coin98",
+  //   icon: Coin98,
+  //   connectorId: "injected",
+  //   priority: 999,
+  // },
 ];
